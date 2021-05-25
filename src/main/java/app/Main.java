bc5ab4c433800583bc37cea2e504b255cc3acb52
NeoCortex63 <64097070+NeoCortex63@users.noreply.github.com>
2021-05-25 16:28:04 +0200

    Update Main.java
    replace code with the ternary operator

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -15,17 +15,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         if (args.length != 1) System.out.println("Incorrect input");
-
+        
         else {
+            
             pathToFile = args[0];
 
-            AnagramFinder anagramFinder = new AnagramFinderImpl();
+            AnagramFinder anagramFinder =  isFileBiggerThen100MB() ? new BigFileAnagramFinder() : new AnagramFinderImpl();
 
-            if (isFileBiggerThen100MB()){
-                anagramFinder = new BigFileAnagramFinder();
-            }
             anagramFinder.printAllAnagrams(pathToFile);
-            }
+        }
     }
 
     private static boolean isFileBiggerThen100MB() throws IOException{
